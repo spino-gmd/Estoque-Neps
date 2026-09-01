9d28573b13a4a19bfb994d6416e2f07d2bde0202
spino-gmd <davimgoulart@gmail.com>
2026-09-01 18:48:06 -0300

    testando

--- stock.cpp
+++ stock.cpp
@@ -2,10 +2,38 @@
 using namespace std;
 
 int main() {
-    //variáveis
+    // coloca as variáveis aqui
     int menu;
     char opt;
     FILE *inv = fopen("inventory.txt", "w");
+
+    //coloca o código dentro do while, é pra fazer o loop
+    while(1){
+    cout << "================================" << endl;
+    cout << "Controle de Estoque" << endl;
+    cout << "================================" << endl;
+    cout << "Selecione uma opção:" << endl;
+    cout << "1. Adicionar Item" << endl;
+    cout << "2. Remover Item" << endl;
+    cout << "3. Listar Estoque" << endl;
+    cout << "4. Sair" << endl;
+    cout << "Opção:";
+
+    cin >> menu;
+    switch(menu){
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        default:
+        
+    }
+
+    }
     
     return 0;
 }
